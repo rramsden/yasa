52c45592ea36e2f0d9e43330ec7029d32d18588d
Richard Ramsden <richard@rramsden.ca>
2012-10-07 16:13:22 -0700

    Cleanup cowboy routes

--- apps/yasa/src/yasa_app.erl
+++ apps/yasa/src/yasa_app.erl
@@ -11,7 +11,6 @@
 
 start(_StartType, _StartArgs) ->
     ok = start_web_server(),
-    ok = check_retentions(),
     yasa_sup:start_link().
 
 stop(_State) ->
@@ -29,29 +28,21 @@ start_web_server() ->
     Dispatch = [
             %% {Host, list({Path, Handler, Opts})}
             {'_', [
-                    {[<<"api">>, action], yasa_http_api_handler, []},
-                    {[<<"wsapi">>], bullet_handler, [{handler, yasa_ws_api_handler}]},
-                    {[<<"assets">>, '...'], cowboy_http_static,
-                     [{directory, {priv_dir, yasa, [<<"www/assets">>]}},
-                      {mimetypes, [
-                                    {<<".css">>, [<<"text/css">>]},
-                                    {<<".js">>, [<<"application/javascript">>]},
-                                    {<<".png">>, [<<"image/png">>]}]}]},
-                    {[], yasa_default_handler, []},
-                    {[<<"ws">>], yasa_default_handler, []}
-                    ]}
-            ],
+                {[<<"api">>, <<"v1">>, action], yasa_web_h_http, []},
+                {[<<"wsapi">>, <<"v1">>], bullet_handler, [{handler, yasa_web_h_ws}]},
+                {[<<"assets">>, '...'], cowboy_http_static,
+                 [{directory, {priv_dir, yasa, [<<"www/assets">>]}},
+                  {mimetypes, [
+                                {<<".css">>, [<<"text/css">>]},
+                                {<<".js">>, [<<"application/javascript">>]},
+                                {<<".png">>, [<<"image/png">>]}]}]},
+                {[], yasa_web_h_static, []}
+            ]}
+    ],
+
     %% Name, NbAcceptors, Transport, TransOpts, Protocol, ProtoOpts
     cowboy:start_listener(my_http_listener, 16,
                           cowboy_tcp_transport, [{port, Port}],
                           cowboy_http_protocol, [{dispatch, Dispatch}]
                          ),
     ok.
-
-%% @private check if the retentions are define in config file and make
-%% sure the are not relatively primary
-check_retentions() ->
-    case application:get_env(yasa, retentions) of 
-        undefined -> throw("Please define retentions");
-        {ok, Rets} -> Rets
-    end.
