40595d2fa1421d0ea453ef1f3e0fbc4ed5474e83
Richard Ramsden <richard@rramsden.ca>
2012-10-07 14:07:36 -0700

    Don't use tabs use spaces!?

--- apps/yasa/src/yasa_app.erl
+++ apps/yasa/src/yasa_app.erl
@@ -9,14 +9,11 @@
 %% Application callbacks
 %% ===================================================================
 
--spec start(_,_) ->  {'error',_} | {'ok',pid()}.
 start(_StartType, _StartArgs) ->
-	%ok = start_web_server(),
-	% don't start with out properly defined retentions
-	%ok = check_retentions(),
+    ok = start_web_server(),
+    ok = check_retentions(),
     yasa_sup:start_link().
 
--spec stop(_) -> 'ok'.
 stop(_State) ->
     ok.
 
@@ -24,48 +21,37 @@ stop(_State) ->
 %%% Internal functions
 %%%===================================================================
 
-%% @private start the web server(cowboy) on the specified port of 8080
-%% by default
 start_web_server() ->
-	Port = case application:get_env(yasa, port) of 
+    Port = case application:get_env(yasa, port) of 
         {ok, P} -> P;
         undefined -> 8080
     end,
-	Dispatch = [
-	    %% {Host, list({Path, Handler, Opts})}
-	    {'_', [
-	    	{[<<"api">>, action], yasa_http_api_handler, []},
-	    	{[<<"wsapi">>], bullet_handler, [{handler, yasa_ws_api_handler}]},
-	    	{[<<"assets">>, '...'], cowboy_http_static,
-    			[{directory, {priv_dir, yasa, [<<"www/assets">>]}},
-    			{mimetypes, [
-          			{<<".css">>, [<<"text/css">>]},
-          			{<<".js">>, [<<"application/javascript">>]},
-          			{<<".png">>, [<<"image/png">>]}]}]},
-	    	{[], yasa_default_handler, []},
-	    	{[<<"ws">>], yasa_default_handler, []}
-		]}
-	],
-	%% Name, NbAcceptors, Transport, TransOpts, Protocol, ProtoOpts
-	cowboy:start_listener(my_http_listener, 16,
-	    cowboy_tcp_transport, [{port, Port}],
-	    cowboy_http_protocol, [{dispatch, Dispatch}]
-	),
-	ok.
+    Dispatch = [
+            %% {Host, list({Path, Handler, Opts})}
+            {'_', [
+                    {[<<"api">>, action], yasa_http_api_handler, []},
+                    {[<<"wsapi">>], bullet_handler, [{handler, yasa_ws_api_handler}]},
+                    {[<<"assets">>, '...'], cowboy_http_static,
+                     [{directory, {priv_dir, yasa, [<<"www/assets">>]}},
+                      {mimetypes, [
+                                    {<<".css">>, [<<"text/css">>]},
+                                    {<<".js">>, [<<"application/javascript">>]},
+                                    {<<".png">>, [<<"image/png">>]}]}]},
+                    {[], yasa_default_handler, []},
+                    {[<<"ws">>], yasa_default_handler, []}
+                    ]}
+            ],
+    %% Name, NbAcceptors, Transport, TransOpts, Protocol, ProtoOpts
+    cowboy:start_listener(my_http_listener, 16,
+                          cowboy_tcp_transport, [{port, Port}],
+                          cowboy_http_protocol, [{dispatch, Dispatch}]
+                         ),
+    ok.
 
 %% @private check if the retentions are define in config file and make
 %% sure the are not relatively primary
-%check_retentions() ->
-%    case application:get_env(yasa, retentions) of 
-%        undefined -> throw("Please define retentions");
-%        {ok, Rets} ->  check_retention_divisibility(Rets)
-%    end.
-%
-%check_retention_divisibility([{FirstStep, _} | Tail]) ->
-%	Fun = fun({StepSize, _}) ->
-%		case StepSize rem FirstStep of
-%			0 -> ok;
-%			_ -> throw("Retentions are relatively primary")
-%		end
-%	end,
-%	lists:foreach(Fun, Tail). 
+check_retentions() ->
+    case application:get_env(yasa, retentions) of 
+        undefined -> throw("Please define retentions");
+        {ok, Rets} -> Rets
+    end.
