1d4241c8d8364bb1cb0150651fc66cc13d3e82d6
Richard Ramsden <richard@rramsden.ca>
2012-10-13 23:49:08 -0700

    Add API call for finding keys

--- apps/yasa/src/yasa.erl
+++ apps/yasa/src/yasa.erl
@@ -1,5 +1,10 @@
 -module(yasa).
--export([counter/2, gauge/2, get/3]).
+-export([
+    counter/2,
+    gauge/2,
+    get/3,
+    walk_directory_tree/1,
+    keys/0]).
 
 %%===================================================================
 %% Public API
@@ -21,6 +26,10 @@ get(Key, Start, End) ->
             Ref
     end,
     gen_server:call(Pid, {get, Start, End}).
+
+keys() ->
+    Root = [code:priv_dir(yasa), "/storage/*"],
+    lists:flatten([walk_directory_tree(Root)]).
 
 %%===================================================================
 %% Internal
@@ -46,3 +55,22 @@ create(Type, Key) ->
     {ok, Pid} = Reply = yasa_rrd_sup:start_child(Type, Key),
     yasa_pid_store:insert(Key, Pid),
     Reply.
+
+walk_directory_tree(Root) ->
+    lists:map(fun(Elem) ->
+        case filelib:is_dir(Elem) of
+            true ->
+                walk_directory_tree([Elem, "/*"]);
+            false ->
+                get_key_from_path(Elem)
+        end
+    end, filelib:wildcard(Root)).
+
+get_key_from_path(Path) ->
+    FlatPath = lists:flatten(Path), 
+    case re:run(FlatPath, <<"storage\\/(?<NAME>.+)\.yasa$">>, [{capture, ['NAME'], binary}]) of
+        {match, [Name]} ->
+            re:replace(Name, "\\/", ".", [global, {return, binary}]);
+        nomatch ->
+            []
+    end.
